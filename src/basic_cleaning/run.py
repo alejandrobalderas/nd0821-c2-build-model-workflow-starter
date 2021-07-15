b72b9defa2279d39944d4dd577fd04ca265c8973
Alejandro Balderas <alejandro@balderas.de>
2021-07-15 09:04:26 +0200

    Add geolocation cleaning in basic_cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,8 @@ def go(args):
     logger.info("Cleansing Data")
     # Drop outliers
     idx = df['price'].between(args.min_price, args.max_price)
+    idx = df['longitude'].between(-74.25, -
+                                  73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
